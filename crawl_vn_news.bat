5c51b1c111f74bc1e06c062b21b1c2e551b69b78
Vu Anh <brother.rain.1024@gmail.com>
2017-05-09 09:27:03 +0700

    update data

--- crawl_vn_news.bat
+++ crawl_vn_news.bat
@@ -1,3 +1,6 @@
 python vn_news/pipelines/pre_crawl.py
 scrapy runspider vn_news/spiders/baomoi.py -o vn_news/raw_data/data.jl -t jsonlines --set HTTPCACHE_ENABLED=0
 python vn_news/pipelines/process_data.py
+git add -A
+git commit -m "update data"
+git push origin master
