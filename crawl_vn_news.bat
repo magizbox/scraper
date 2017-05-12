01070b2c3d485f6695d9464f62993fcd90d0bc65
Vu Anh <brother.rain.1024@gmail.com>
2017-05-12 11:15:08 +0700

    update data

--- crawl_vn_news.bat
+++ crawl_vn_news.bat
@@ -1,6 +1,7 @@
 python vn_news/pipelines/pre_crawl.py
 scrapy runspider vn_news/spiders/baomoi.py -o vn_news/raw_data/data.jl -t jsonlines --set HTTPCACHE_ENABLED=0
 python vn_news/pipelines/process_data.py
+python vn_news/pipelines/report.py
 git add -A
 git commit -m "update data"
 git push origin master
---
+++ vn_news/pipelines/report.py
@@ -0,0 +1,12 @@
+from os import listdir
+from jinja2 import Template
+from os.path import dirname, join
+
+current_directory = dirname(__file__)
+template = open(join(current_directory, "report.template")).read().decode("utf-8")
+template = Template(template)
+num_articles = len(listdir(join(dirname(current_directory), "data")))
+content = template.render(num_articles=num_articles)
+report_file = join(dirname(dirname(current_directory)), "README.md")
+open(report_file, "w").write(content.encode("utf-8"))
+print(content)
