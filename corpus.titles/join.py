d035053a92d8e4bbbbce4852253c4f7d59b7cbb2
Vu Anh <brother.rain.1024@gmail.com>
2017-05-24 10:16:01 +0700

    add data folder

--- corpus.titles/join.py
+++ corpus.titles/join.py
@@ -1,3 +1,6 @@
+from os.path import dirname, join
+
+
 def join(file_names, output_file):
     with open(output_file, 'w') as outfile:
         for file_name in file_names:
@@ -7,7 +10,8 @@ def join(file_names, output_file):
 
 
 if __name__ == '__main__':
-    file_names = ["corpus-title_300000.txt", "corpus-title_600000.txt", "corpus-title_900000.txt",
-                  "corpus-title_1200000.txt", "corpus-title_1500000.txt"]
+    data_folder = join(dirname(__file__), "data")
+    file_names = listdir(data_folder)
+    file_names = [join(data_folder, f) for f in file_names]
     output_file = "corpus-title-merge.txt"
     join(file_names, output_file)
--- corpus.titles/split.py
+++ corpus.titles/split.py
@@ -1,4 +1,5 @@
 from itertools import izip_longest
+from os.path import dirname, join
 
 
 def grouper(n, iterable, fillvalue=None):
@@ -11,7 +12,9 @@ if __name__ == '__main__':
     chunk_size = 300000  # lines
     input_file = 'corpus-title.txt'
     n = chunk_size
+    current_dir = dirname(__file__)
+    data_dir = join(current_dir, "data")
     with open(input_file) as f:
         for i, g in enumerate(grouper(n, f, fillvalue=''), 1):
-            with open('corpus-title_{0}.txt'.format(i * n), 'w') as fout:
+            with open(join(data_dir, 'corpus-title_{0}.txt'.format(i * n)), 'w') as fout:
                 fout.writelines(g)
