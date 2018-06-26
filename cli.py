3387ada618569fd73517092f2215a89398d8f7ee
Vu Anh <anhv.ict91@gmail.com>
2018-06-26 11:18:01 +0700

    update cli

--- cli.py
+++ cli.py
@@ -2,7 +2,7 @@ from scrapy import cmdline
 import os
 
 if __name__ == '__main__':
-    cmdline.execute("scrapy runspider vn_news\\spiders\\baomoi.py -o vn_news\\raw_data\\data.jl -t jsonlines".split())
+    cmdline.execute("scrapy runspider vn_news/spiders/baomoi.py -o vn_news/raw_data/data.jl -t jsonlines".split())
 
 # debug
 # cmdline.execute("scrapy runspider vn_news\\spiders\\baomoi_article.py -o vn_news\\raw_data\\data.jl -t jsonlines".split())
